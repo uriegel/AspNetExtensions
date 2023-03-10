bf295b30571d2339868736dc25aea69eeb4a20fd
Uwe Riegel <uriegel@hotmail.de>
2023-03-10 17:01:12 +0100

    Sse

--- Tester/Program.cs
+++ Tester/Program.cs
@@ -1,2 +1,58 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using AspNetExtensions;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+
+using static AspNetExtensions.Core;
+
+var sse = new Sse<RendererEvent>();
+// Theme.StartThemeDetection(sse.SetTheme);
+
+WebApplication
+    .CreateBuilder(args)
+    .ConfigureWebHost(webHostBuilder =>
+        webHostBuilder
+            .ConfigureKestrel(options => options.ListenAnyIP(19999))
+            .ConfigureServices(services =>
+                services
+                    .AddCors()
+                    .AddResponseCompression())
+            .ConfigureLogging(builder =>
+                builder
+                    .AddFilter(a => a == LogLevel.Warning)
+                    .AddConsole()
+                    .AddDebug()))
+    .Build()
+    .WithResponseCompression()
+    .With(async (context, next) =>
+    {
+        if (context.Request.Path == "/")
+        {
+            await context.Response.WriteAsync("Terminal Middleware.");
+            return;
+        }
+        await next(context);
+    })
+    .WithMapGet("/test", () => "Das ist der Test")
+    .WithMapGet("/filmseite", context =>
+        {
+            context.Response.Headers.ContentType = "text/html";
+            return context.Response.WriteAsync(VideoPage.Value);
+        })
+    .WithMapGet("/film", context => context.StreamRangeFile("/home/uwe/Videos/Buster Keaton - Der Navigator.mp4"))
+    .WithMapGet("/commander/sse", h => sse.Start(h))
+    .WithMapGet("/json/{name:alpha}", async context =>
+        {
+            var name = context.Request.RouteValues["name"];
+            await context.Response.WriteAsJsonAsync(new { Message = $"Hello {name}", Mist = (string?)null }, JsonWebDefaults);
+        })
+    .WithCors(builder =>
+        builder
+            .WithOrigins("http://localhost:3000")
+            .AllowAnyHeader()
+            .AllowAnyMethod())
+    .WithRouting()
+    .Run();
+
+record RendererEvent(string Theme);
+// TODO Events with inheritance
---
+++ Tester/VideoPage.cs
@@ -0,0 +1,13 @@
+static class VideoPage
+{
+    public static string Value { get; } = """
+    <html>
+    <body>
+    <h1>Test</h1>
+    <video controls autoPlay
+            src="/video" />        
+
+    </body>
+    </html
+    """;
+}
