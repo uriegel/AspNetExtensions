76482d20d7154c2d021872328bca52f3311868f9
Uwe Riegel <uriegel@hotmail.de>
2023-08-11 14:24:14 +0200

    WebApplicationWithHost extensions

--- Tester/Program.cs
+++ Tester/Program.cs
@@ -61,6 +61,62 @@ WebApplication
     .WithJsonPost<Cmd1Param, Cmd1Result>("json/cmd1", JsonRequest1)
     .WithRouting()
     .WithFileServer("/web", "webroot")
+    .Start();
+
+WebApplication
+    .CreateBuilder(args)
+    .ConfigureWebHost(webHostBuilder =>
+        webHostBuilder
+            .ConfigureKestrel(options => options.ListenAnyIP(2001))
+            .ConfigureServices(services =>
+                services
+                    .When(true, s => s.AddCors())
+                    .AddResponseCompression())
+            .ConfigureLogging(builder =>
+                builder
+                    .AddFilter(a => a == LogLevel.Warning)
+                    .AddConsole()
+                    .AddDebug()))
+    .Build()
+    .WithResponseCompression()
+    .WithHost("illmatic")
+        .WithMapGet("/test1i", () => "Test 1 illmatic")
+        .WithMapGet("/test2i", () => "Test 2 illmatic")
+        .GetApp()
+    .WithHost("localhost")
+        .WithMapGet("/test1l", () => "Test 1 localhost")
+        .WithMapGet("/test2l", () => "Test 2 localhost")
+        .GetApp()
+
+    .WithMapGet("/test1", () => "Test 1")
+    .WithMapGet("/test2", () => "Test 2")
+    .WithMapGet("/cinema", context =>
+        {
+            context.Response.Headers.ContentType = "text/html";
+            return context.Response.WriteAsync(VideoPage.Value);
+        })
+    .WithMapGet("/video", context => context.StreamRangeFile("/home/uwe/Videos/Buster Keaton - Der Navigator.mp4"))
+    .WithMapGet("/requests/icon", async context => 
+        {
+            var qs = context.Request.Query;
+            var test = qs["path"].ToString();
+            using var imageFile = File.OpenRead(Path.Combine(System.Environment.CurrentDirectory, "webroot", "Bild188.JPG"));
+            await context.SendStream(imageFile, startTime, imageFile.Name);
+        })
+    .WithMapGet("/json/{name:alpha}", async context =>
+        {
+            var name = context.Request.RouteValues["name"];
+            await context.Response.WriteAsJsonAsync(new { Message = $"Hello {name}", Mist = (string?)null }, JsonWebDefaults);
+        })
+    .WithSse("/sse/test", sseEventSource)
+    .When(true, app => app.WithCors(builder =>
+        builder
+            .WithOrigins("http://localhost:3000")
+            .AllowAnyHeader()
+            .AllowAnyMethod()))
+    .WithJsonPost<Cmd1Param, Cmd1Result>("json/cmd1", JsonRequest1)
+    .WithRouting()
+    .WithFileServer("/web", "webroot")
     .Run();
 
 Task<Cmd1Result> JsonRequest1(Cmd1Param param)
