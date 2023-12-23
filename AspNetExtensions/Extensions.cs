5957dfc2e3c9fd1c3445bcfd82e739f6619df634
Uwe Riegel <uriegel@hotmail.de>
2023-12-23 12:33:53 +0100

    unecessary generic parameter

--- AspNetExtensions/Extensions.cs
+++ AspNetExtensions/Extensions.cs
@@ -133,7 +133,7 @@ public static class Extensions
         });
 
 
-    public static WebApplication WithJsonPost<T, TResult, TE>(this WebApplication webApp, string path, Func<AsyncResult<TResult, TE>> onJson)
+    public static WebApplication WithJsonPost<TResult, TE>(this WebApplication webApp, string path, Func<AsyncResult<TResult, TE>> onJson)
             where TResult : notnull
             where TE : RequestError
         => webApp.WithMapPost(path, async context =>
